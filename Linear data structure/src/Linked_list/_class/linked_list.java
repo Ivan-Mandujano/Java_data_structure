f5f8654008b3e4665f6294c7f0ac197c54ea3cd4
°Ivan-Mandujano° <ivanmandujano594@gmail.com>
2023-10-12 20:01:20 -0600

    Simple linked list complete
    Simple Linked list, added the metod Search by position, search by data, pop the head, print the whole list and List_size

--- Linear data structure/src/Linked_list/_class/linked_list.java
+++ Linear data structure/src/Linked_list/_class/linked_list.java
@@ -8,6 +8,19 @@ public class linked_list {
 		head = null;
 	}
 	//Methods
+    public int List_size() {
+    	if (head==null) {
+    		return -1;
+    	}else {
+    		Node newNode = head;
+    		int i = 0;
+	    	do {
+	    		newNode = newNode.getNext();
+	    		i++;
+	    	}while(newNode!=null);
+	    	return i;
+    	}
+    }
     public void add(Object data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -18,9 +31,67 @@ public class linked_list {
         }
     } 
     public void Print() {
-    	System.out.println("Primer dato: " + head.getData());
-    	//Get the next node
-    	Node siguiente = new Node(head.getNext());
-    	System.out.println("Segundo dato: "+ siguiente);
+    	if (head==null) {
+    		System.out.println("Error: No data in the list");
+    	}else {
+    		Node newNode = head;
+    		int i = 0;
+	    	do {
+	    		Object data = newNode.getData();
+	    		System.out.println("Position " + i + ": " + data);
+	    		newNode = newNode.getNext();
+	    		i++;
+	    	}while(newNode!=null);
+    	}
+    }
+    public Object Search_Pos(int pos) {
+    	if (head==null) {
+    		System.out.println("Error: No data in the list");
+    	}else if(pos>List_size() || pos<0){
+    		System.out.println("Error: Out of index");
+    	}else {
+    		Node newNode = head;
+    		int i = 0;
+    		boolean found = false;
+	    	do {
+	    		Object data = newNode.getData();
+	    		newNode = newNode.getNext();
+	    		if(i==pos) {
+	    			found = true;
+	    			return data;
+	    		}
+	    		i++;
+	    	}while(newNode!=null && found == false);
+    	}
+		return -1;
+    }
+    public int Search_Data(Object searched) {
+    	if (head==null) {
+    		System.out.println("Error: No data in the list");
+    	}else {
+    		Node newNode = head;
+    		int i = 0;
+    		boolean found = false;
+	    	do {
+	    		Object data = newNode.getData();
+	    		newNode = newNode.getNext();
+	    		if(data==searched) {
+	    			found = true;
+	    			return i;
+	    		}
+	    		i++;
+	    	}while(newNode!=null && found == false);
+    	}
+		return -1;
+    }
+    public void Pop() {
+    	if (head==null) {
+    		System.out.println("Error: No data in the list");
+    	}else {
+    		System.out.println("Pop from the list: "+ head.getData());
+    		Node newNode = head.getNext();
+            head.setNext(null);
+    		head = newNode;
+    	}
     }
 }
--- Linear data structure/src/Linked_list/_class/main.java
+++ Linear data structure/src/Linked_list/_class/main.java
@@ -3,9 +3,18 @@ package Linked_list._class;
 public class main {
 	public static void main(String[] args) {
 		linked_list lista = new linked_list();
-        
+        lista.Print();
         lista.add("Primer dato");
         lista.add("Segundo dato");
+        lista.add("Tercer dato");
+        lista.add("Cuarto dato");
 		lista.Print();
+		lista.Pop();
+		lista.add("cuarto dato 2");
+		lista.Print();
+		System.out.println(lista.Search_Pos(2));
+		System.out.println(lista.Search_Data("cuarto dato 2"));
+
+
 	}
 }
